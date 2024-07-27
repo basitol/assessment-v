d1b12d872b4ee96eed0c85c099e6871b9e578993
Basit Quadri <basitolaitan27@gmail.com>
2024-07-27 03:11:13 +0100

    fix deployment errors

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -8,16 +8,14 @@ const Page = () => {
   const breadcrumbItems = [{name: 'Account', href: '/account'}];
 
   return (
-    <Layout
-      children={
-        <div className='px-4'>
-          <div className='py-2'>
-            <Breadcrumb items={breadcrumbItems} />
-          </div>{' '}
-          <h2 className='mb-4 text-2xl font-bold text-[#1D2739]'>Account</h2>
-        </div>
-      }
-    />
+    <Layout>
+      <div className='px-4'>
+        <div className='py-2'>
+          <Breadcrumb items={breadcrumbItems} />
+        </div>{' '}
+        <h2 className='mb-4 text-2xl font-bold text-[#1D2739]'>Account</h2>
+      </div>
+    </Layout>
   );
 };
 
--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -23,40 +23,38 @@ const UsersRoles = () => {
   ];
 
   return (
-    <Layout
-      children={
-        <div className='px-4'>
-          <div className='py-2'>
-            <Breadcrumb items={breadcrumbItems} />
-          </div>{' '}
-          <div className='mb-6'>
-            <h2 className='mb-4 text-2xl font-bold text-[#1D2739]'>
-              Users & Roles
-            </h2>
-            <p className='text-[#98A2B3]'>Manage all users in your business</p>
-          </div>
-          <TabBar
-            tabs={['Users', 'Roles']}
-            activeTab={activeTab}
-            onTabClick={setActiveTab}
-          />
-          <div className='bg-white mt-4 p-4 rounded-md shadow'>
-            {activeTab === 'Users' && (
-              <>
-                {loading ? (
-                  <p>Loading...</p>
-                ) : error ? (
-                  <p className='text-red-500'>{error}</p>
-                ) : (
-                  <DataTable columns={columns} data={filteredUsers} />
-                )}
-              </>
-            )}
-            {activeTab === 'Roles' && <p>No Roles availabble</p>}
-          </div>
+    <Layout>
+      <div className='px-4'>
+        <div className='py-2'>
+          <Breadcrumb items={breadcrumbItems} />
+        </div>{' '}
+        <div className='mb-6'>
+          <h2 className='mb-4 text-2xl font-bold text-[#1D2739]'>
+            Users & Roles
+          </h2>
+          <p className='text-[#98A2B3]'>Manage all users in your business</p>
         </div>
-      }
-    />
+        <TabBar
+          tabs={['Users', 'Roles']}
+          activeTab={activeTab}
+          onTabClick={setActiveTab}
+        />
+        <div className='bg-white mt-4 p-4 rounded-md shadow'>
+          {activeTab === 'Users' && (
+            <>
+              {loading ? (
+                <p>Loading...</p>
+              ) : error ? (
+                <p className='text-red-500'>{error}</p>
+              ) : (
+                <DataTable columns={columns} data={filteredUsers} />
+              )}
+            </>
+          )}
+          {activeTab === 'Roles' && <p>No Roles availabble</p>}
+        </div>
+      </div>
+    </Layout>
   );
 };
 
